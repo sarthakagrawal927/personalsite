1aa7d67ceeb1ae58dd6f6e7e928d130beb927266
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2024-01-01 21:50:21 +0530

    Cleanup website a bit

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,19 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Significant Hobbies Blog',
+  author: 'Sarthak Agrawal',
+  headerTitle: 'Significant Hobbies',
+  description: 'A blog on philosophy, psychology, productivity, startups, technology, and more.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://blog.significanthobbies.com/',
+  siteRepo: 'https://github.com/sarthakagrawal927/personalsite',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'sarthakagrawal927@gmail.com',
+  github: 'https://github.com/sarthakagrawal927',
+  twitter: 'https://twitter.com/@sarthakcodes',
+  linkedin: 'https://www.linkedin.com/in/sarthakagrawal927/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
